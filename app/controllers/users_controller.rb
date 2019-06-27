a673b74475b22890a9925472999b4b023d82f99b
thanh binh <le.thanh.binh@sun-asterisk.com>
2019-06-27 11:44:23 +0700

    chapter 7: sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,28 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def show
+    redirect_to signup_path unless @user = User.find_by(params[:id])
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash.now[:success] = t ".create_success"
+      redirect_to @user
+    else
+      flash.now[:danger] = t ".create_error"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,15 @@ class User < ApplicationRecord
   before_save :downcase_mail
 
   validates :name,  presence: true,
-            length: {maximum: Settings[:user][:name_max_length]}
+            length: {maximum: Settings.user.name_max_length}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
-            length: {maximum: Settings[:user][:mail_max_length]},
+            length: {maximum: Settings.user.mail_max_length},
             format: {with: VALID_EMAIL_REGEX},
             uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, presence: true,
-            length: {minimum: Settings[:user][:pass_min_length]}
+            length: {minimum: Settings.user.pass_min_length}
 
   private
 
